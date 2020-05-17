9c9b9c8ca8c2a39499c53d2f229d6b459cb11af2
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-05-17 11:26:42 -0400

    added text editor

--- shell.py
+++ shell.py
@@ -1,12 +1,54 @@
 import basic
 import interpreter
+import re
 
 inter = interpreter.Interpreter()
 
+in_editor = False
+line = 1
+
 while True:
-    text = input('rickroll > ')
-    if text.strip() == 'run':
-        inter.execute()
-        inter = interpreter.Interpreter()
-    else :
-        inter.append(text)
+    if in_editor:
+        text = input('line ' + str(line) + ' > ')
+        if text.strip() == 'exit':
+            in_editor = False
+        else:
+            inter.append(text)
+            line += 1
+    else:
+        text = input('rickroll > ').strip()
+        if text == 'edit':
+            in_editor = True
+        elif text == 'run':
+            inter.execute()
+        elif re.match('^delete \d+$', text):
+            index = int(text[7 : ])
+            if index < line:
+                inter.pop(index)
+                line -= 1
+            else:
+                print('No such index')
+        elif re.match('^insert \d+$', text):
+            index = int(text[7 : ])
+            if index <= line:
+                text = input('line ' + str(index) + ' > ')
+                inter.insert(index, text)
+                line += 1
+            else:
+                print('No such index')
+        elif re.match('^replace \d+$', text):
+            index = int(text[8 : ])
+            if index < line:
+                text = input('line ' + str(index) + ' > ')
+                inter.replace(index, text)
+                line += 1
+            else:
+                print('No such index')
+        elif text == 'display':
+            lines = inter.text
+            index = 1
+            for program_line in lines:
+                print('line ' + str(index) + ' > ' + program_line)
+                index += 1
+        elif text == 'new':
+            inter = interpreter.Interpreter()
