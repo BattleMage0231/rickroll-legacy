979512ffa86f26207fe641cb358819be81666bb2
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2020-05-17 12:48:26 -0400

    add text editor and colored text

--- shell.py
+++ shell.py
@@ -1,6 +1,19 @@
 import basic
 import interpreter
 import re
+import os
+
+os.system("cls") # windows workaround to fix color formatting bug
+
+# color constants
+COLOR_RED = '\033[91m'
+COLOR_GREEN = '\033[92m'
+COLOR_YELLOW = '\033[93m'
+COLOR_END = '\033[0m'
+
+# format text in color
+def in_color(text, color):
+    return color + text + COLOR_END
 
 inter = interpreter.Interpreter()
 
@@ -9,14 +22,14 @@ line = 1
 
 while True:
     if in_editor:
-        text = input('line ' + str(line) + ' > ')
+        text = input(in_color('line ' + str(line) + ' > ', COLOR_YELLOW))
         if text.strip() == 'exit':
             in_editor = False
         else:
             inter.append(text)
             line += 1
     else:
-        text = input('rickroll > ').strip()
+        text = input(in_color('rickroll > ', COLOR_GREEN)).strip()
         if text == 'edit':
             in_editor = True
         elif text == 'run':
@@ -27,28 +40,30 @@ while True:
                 inter.pop(index)
                 line -= 1
             else:
-                print('No such index')
+                print(in_color('No such index', COLOR_RED))
         elif re.match('^insert \d+$', text):
             index = int(text[7 : ])
             if index <= line:
-                text = input('line ' + str(index) + ' > ')
+                text = input(in_color('line ' + str(index) + ' > ', COLOR_YELLOW))
                 inter.insert(index, text)
                 line += 1
             else:
-                print('No such index')
+                print(in_color('No such index', COLOR_RED))
         elif re.match('^replace \d+$', text):
             index = int(text[8 : ])
             if index < line:
-                text = input('line ' + str(index) + ' > ')
+                text = in_color('line ' + str(index) + ' > ', COLOR_YELLOW)
                 inter.replace(index, text)
                 line += 1
             else:
-                print('No such index')
+                print(in_color('No such index', COLOR_RED))
         elif text == 'display':
             lines = inter.text
             index = 1
             for program_line in lines:
-                print('line ' + str(index) + ' > ' + program_line)
+                print(in_color('line ' + str(index) + ' > ', COLOR_YELLOW) + program_line)
                 index += 1
         elif text == 'new':
             inter = interpreter.Interpreter()
+        elif text == 'exit':
+            exit()
